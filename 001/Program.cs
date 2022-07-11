e98c54854cbebe0d68c285e2e810daae77df7fef
YA88 <lexu4.122@yandex.ru>
2022-07-11 19:38:31 +0500

    Добавили решение задачи№1

---
+++ 001/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+int a = 1245;
+Console.WriteLine(a);
+Console.WriteLine("Hello World!");
+Console.WriteLine(13%4);
+string s = "Hello"+"abc";//конкотенация строк (склеивание)
+Console.WriteLine(s); 
+char c = 'a';
+System.Console.WriteLine(int.MaxValue);
+System.Console.WriteLine(double.MaxValue);
+*/
+// Задача №1
+int a=4;
+int b;
+b=a*a;
+//Console.WriteLine(b);
+Console.WriteLine($"{a}^2={b}");
