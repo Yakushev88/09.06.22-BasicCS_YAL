7a12f52614e572d8b84c8866473fe25dd32b9e4a
YA88 <lexu4.122@yandex.ru>
2022-07-13 19:27:11 +0500

    Добавил решение задачи №7

---
+++ 007/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача №7
+// Выяснить является ли число чётным.
+int a;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+if (a%2==0) // ПРОВЕРЯЕМ ДЕЛИТЬСЯ ЛИ ЧИСЛО НА 2 БЕЗ ОСТАТКА
+    {
+    System.Console.Write("Число является чётным");
+    }
+else
+System.Console.Write("Число является не чётным");
