56da5cd3f784deb7939757ce44061534acd9b706
YA88 <lexu4.122@yandex.ru>
2022-07-13 21:25:32 +0500

    Добавил решение задачи №6

--- 006/Program.cs
+++ 006/Program.cs
@@ -1,8 +1,9 @@
 ﻿// Задача №6
-// Написать программу вычисления значения функции y = sin(a). 
+// Написать программу вычисления значения функции 
+//y = sin(a). 
 //(Класс Math)
 
-int a=45;
-int y;
-Math.Sin(a);
-System.Console.WriteLine(y);
+double x,y;
+x=3.14;
+y=Math.Sin(x);
+System.Console.WriteLine($"sin{x}={y}");
