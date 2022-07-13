cf114bcce29e180d7d758cb4ac5e4f7ba28aeb9e
YA88 <lexu4.122@yandex.ru>
2022-07-13 18:55:12 +0500

    Начал пробовать решать пятую задачу

---
+++ 005/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача № 5
+//С клавиатуры вводятся три числа. 
+//Найти максимальное из трех чисел
+
+int a;b;c;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+s=Console.ReadLine();
+c=Convert.ToInt32(s);
+m=a;
+if(a>b)
+{
+   
+}
+System.Console.WriteLine(Max);
+
+
+/*
+int i=2;
+int N=3;
+int a;b;c;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+s=Console.ReadLine();
+c=Convert.ToInt32(s);
+int Max=a;
+i=1;
+
+if(i<N)
+{
+    if
+    (numbers[i]>Max)
+    (Max=numbers[i]);
+    else
+    i=i+1;
+}
+System.Console.WriteLine(Max);
+*/
