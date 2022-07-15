e2acc65950227be639e23bf58c21d20030e1354a
YA88 <lexu4.122@yandex.ru>
2022-07-15 17:44:21 +0500

    Добавили решение для задачи №19

---
+++ 019/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №19 
+// Проверить истинность утверждения
+// (C# (!X||Y)==!X&!Y))
+//!(X || Y)==!X and !Y
+
+int N=567;
+System.Console.WriteLine($"N={N}");
+bool x,y,f1,f2;
+x=true;
+y=true;
+f1=!(x||y);
+f2=!x&&!y;
+
+System.Console.WriteLine($"{x} {y} {f1} {f2}");
