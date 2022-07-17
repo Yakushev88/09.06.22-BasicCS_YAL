c0f33a9112dee5999ae33371ac678a820a80f79d
YA88 <lexu4.122@yandex.ru>
2022-07-17 11:55:08 +0500

    Ввел условие ввода числа в задаче №15

--- 015/Program.cs
+++ 015/Program.cs
@@ -2,11 +2,12 @@
 // С клавиатуры вводится целое число. Вывести третью цифру числа 
 // или сообщить, что её нет.
 int a;
+System.Console.WriteLine("введите число до 99999999!");
 string? s=Console.ReadLine();
 a=Convert.ToInt32(s);
 if (a>99)
     {
-    System.Console.WriteLine((a/10000)%10);
+    System.Console.WriteLine((a/100000)%10);
     }
 else
     {
