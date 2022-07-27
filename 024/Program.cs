10e6eb6dd978e20b1e4d6a9204e56e1db4f29a2a
YA88 <lexu4.122@yandex.ru>
2022-07-27 19:01:12 +0500

    Добавил решение задачи №24

---
+++ 024/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+double N=20; 
+double i=1;
+double iq=1;
+while(i<=N) 
+{
+    iq=Math.Pow(i,2);
+    System.Console.WriteLine($"{iq} ");
+    i=i+1; 
+}
