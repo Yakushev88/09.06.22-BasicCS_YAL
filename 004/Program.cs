b005d0d9adc78ac8d4b8a83061e7f2c3b0b1da36
YA88 <lexu4.122@yandex.ru>
2022-07-27 15:16:56 +0500

    Добавили решение задачи №4 через массив

--- 004/Program.cs
+++ 004/Program.cs
@@ -35,7 +35,7 @@ if (a==1);
     System.Console.Write("Sunday");
     }
 */
-
+/*
 int dayOfWeek= Convert.ToInt32(Console.ReadLine());
 // int DayOfWeek; - Pascal Case
 if (dayOfWeek==1) System.Console.WriteLine("Monday");
@@ -45,3 +45,35 @@ if (dayOfWeek==4) System.Console.WriteLine("Thursday");
 if (dayOfWeek==5) System.Console.WriteLine("Friday");
 if (dayOfWeek==6) System.Console.WriteLine("Saturday");
 if (dayOfWeek==7) System.Console.WriteLine("Sunday");
+  if (dayOfWeek==1) System.Console.WriteLine("Monday");
+else
+   if (dayOfWeek==2) System.Console.WriteLine("Tuesday");
+   else
+*/
+
+// Swithcase
+/*
+int dayOfWeek= Convert.ToInt32(Console.ReadLine());
+switch(dayOfWeek)
+{
+  case 1:
+        System.Console.WriteLine("Monday");
+        break;
+  case 2:
+        System.Console.WriteLine("Tuestday");
+        break;
+  case 3:
+        System.Console.WriteLine("Wednesday");
+        break;
+  default:
+        System.Console.WriteLine("Wrong");
+        break;
+}
+*/
+// Пропробовать через массивы!!!
+
+System.Console.WriteLine("Введите порядковый номер дня недели: ");
+int numbersDoF= Convert.ToInt32(Console.ReadLine());
+// Задаем строковой массивю Сразу наполнили.
+string[] weekDays = new string[] {"Monday" , "Tuesday" , "Wednesday" , "Thurstday" ,"Friday" , "Saturday" ," Sunday" ,};
+System.Console.WriteLine(weekDays[numbersDoF-1]);
