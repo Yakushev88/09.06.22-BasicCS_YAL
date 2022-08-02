092d2dfee5fe5c2536cc7a8dd2d00d6635a41bce
YA88 <lexu4.122@yandex.ru>
2022-08-02 19:55:11 +0500

    Добавил решение задачи №26

---
+++ 026/Program.cs
@@ -0,0 +1,16 @@
+﻿// 26. Найти сумму чисел от 1 до А(-)
+
+int A=10;
+int[] a=new int[A];
+Random random=new Random();// Вводит случайные числа в тело массива 
+for (int i=1; i<a.Length-1; i++)
+    a[i]=random.Next(0,10);
+
+for (int i=1; i<a.Length-1; i++)
+    System.Console.Write($"{a[i],4}");
+
+int sum=0;
+for (int i=1; i<a.Length;i++)
+    sum=sum+a[i];
+System.Console.WriteLine();
+System.Console.Write(sum);
