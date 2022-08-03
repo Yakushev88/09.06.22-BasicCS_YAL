7ccc04e71300474b55f34350f773d824ce8c4986
YA88 <lexu4.122@yandex.ru>
2022-08-03 20:02:03 +0500

    Добавили решение через подпрограммы

--- 032/Program.cs
+++ 032/Program.cs
@@ -1,11 +1,50 @@
 ﻿// Задача №32
 // Задать массив из 8 элементов и вывести их на экран
-
+/*
 int[] t; //// Объявили масив
 t= new int[8];
  t[0]=10;
  t[1]=12;
+ // Инициализация
  for(int i=0;i<t.Length; i++)
     t[i]=i*12;
+// решение задачи
+
+//вывод результатов на экран
  for(int i=0;i<t.Length; i++)
     System.Console.WriteLine($"a[{i}]={t[i]} ");
+*/
+
+// Решение данной задачи с помощью подпрограммы
+int[] t; //// Объявили масив
+Init(out t,8, max:100);
+ t[0]=10;
+ t[1]=12;
+ // Инициализация
+ for(int i=0;i<t.Length; i++)
+    t[i]=i*12;
+// решение задачи
+
+//вывод результатов на экран
+Print(t);
+/*
+ for(int i=0;i<t.Length; i++)
+    System.Console.WriteLine($"a[{i}]={t[i]} ");
+ */
+void Init(out int[] t, int Length, int min=0, int max=10)
+{
+   t=new int[Length];
+   Random random=new Random();
+   for(int i=0;i<t.Length; i++)
+      t[i]=random.Next(min,max+1);
+}
+
+ // Вывод на экран в виде метода
+
+void Print(int[] t)
+{
+   for(int i=0;i<t.Length; i++)
+   System.Console.WriteLine($"a[{i}]={t[i]}");
+}
+
+
--- 034/Program.cs
+++ 034/Program.cs
@@ -67,7 +67,7 @@ for (int i=0; i<a.Length; i++)
     System.Console.WriteLine($"{a[i],4}");
 */
 
-
+/*
 // Сумма и положительных и отрицательных чисел ВМЕСТЕ
 
 int N=12;
@@ -94,6 +94,70 @@ for (int i=0; i<a.Length; i++)
     System.Console.Write($"{a[i],4}");
 
 
+
+
+ 
+void Init(out int[] t, int Length, int min=0, int max=10)
+{
+   t=new int[Length];
+   Random random=new Random();
+   for(int i=0;i<t.Length; i++)
+      t[i]=random.Next(min,max+1);
+}
+
+ // Вывод на экран в виде метода
+
+void Print(int[] t)
+{
+   for(int i=0;i<t.Length; i++)
+   System.Console.WriteLine($"a[{i}]={t[i]}");
+}
+*/
+
+
+// Колдовство с подпрограммами
+int N=12;
+int[] a;// Описали массив
+Init(out a,12,-9,9); // инициализировали 
+
+// вывод массива (до обработки)
+Print(a);
+
+//решение задачи
+int sumPositive=0;
+for (int i=0; i<a.Length; i++)
+    if (a[i]>0)
+        sumPositive=sumPositive+a[i];
+    
+System.Console.WriteLine(sumPositive);
+
+int sumNegative=0;
+for (int i=0; i<a.Length; i++)
+    if (a[i]<0)
+        //sumNegative=sumNegative+a[i];
+        sumNegative+=a[i];// ТО ЖЕ САМОЕ ЧТО СТРОЧКА ВЫШЕ
+System.Console.WriteLine(sumNegative);
+
+for (int i=0; i<a.Length; i++)
+    System.Console.Write($"{a[i],4}");
+
+void Init(out int[] t, int Length, int min=0, int max=10)
+{
+   t=new int[Length];
+   Random random=new Random();
+   for(int i=0;i<t.Length; i++)
+      t[i]=random.Next(min,max+1);
+}
+
+ // Вывод на экран в виде метода
+
+void Print(int[] t)
+{
+   for(int i=0;i<t.Length; i++)
+   System.Console.WriteLine($"a[{i}]={t[i]}");
+}
+
+void Solve(int[])
 
 
 
