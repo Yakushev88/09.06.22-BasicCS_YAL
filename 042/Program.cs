6b7b6f655e0a9772a08564817ef4f137846989cb
YA88 <lexu4.122@yandex.ru>
2022-08-06 13:26:48 +0500

    Добавили решение задачи №42

---
+++ 042/Program.cs
@@ -0,0 +1,15 @@
+﻿// 42. Выяснить являются ли три 
+//числа сторонами треугольника
+// bool - true or false
+
+double a=6,b=12,c=7;
+System.Console.WriteLine(IsTreangle(a,b,c));
+
+
+
+bool IsTreangle(double a, double b, double c)
+{
+    // Правило математики, что сумма любых сторон 
+    // больше третьей
+    return(a+b>c && b+c>a && a+c>b);
+}
