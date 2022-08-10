c7cb196ee6944a0d40d4f246c670db46c7dee8b4
YA88 <lexu4.122@yandex.ru>
2022-08-10 21:59:13 +0500

    Добавили решения задач на переворот массиваб на вывод четных чисел в определенном порядке

---
+++ EJUGE/001/Program.cs
@@ -0,0 +1,73 @@
+﻿//Вводиться число N, а затем - N чисел. 
+// Выведите N чисел в обратном порядке
+// Входные данные 
+// Выведите N чисел в обратном порядке
+// 100>=N>=2
+// Пример входнеого файла:
+//7
+//2 4 1 3 5 3 1
+// Пример выходнеого файла:
+// 1 3 5 3 1 4 2
+/*
+int n=int.Parse(Console.ReadLine()); // Первая строка
+string s=Console.ReadLine(); //Вторая строка для ввода
+string[] ss=s.Split(' '); // Преобразуем строку в массив строк
+//Преобразуем массив строк в массив int
+int[] a=Array.ConvertAll<string,int>(ss, int.Parse);
+// Решение
+for (int i=0; i< a.Length/2; i++)
+{
+    int tmp =a[i]; // Временная переменная
+    // меняем нулевой на последний
+    a[i] = a[a.Length-i-1]; // -i что бы не выйти за границы массива
+    a[a.Length-i-1]=tmp; // меняем последний на нулевой
+}
+for (int i=0; i < a.Length; i++)// перебор по всему массиву
+{
+   System.Console.Write(a[i]+" "); 
+}
+*/
+/*
+// Через строковой тип
+// Преворачиваем строковой массив
+
+int n=int.Parse(Console.ReadLine()); // Первая строка
+string s=Console.ReadLine(); //Вторая строка для ввода
+string[] a=s.Split(' ',StringSplitOptions.RemoveEmptyEntries); // Преобразуем строку в массив строк
+//Преобразуем массив строк в массив int
+//int[] a=Array.ConvertAll<string,int>(ss, int.Parse);
+// Решение
+for (int i=0; i< a.Length/2; i++)
+{
+    string tmp =a[i]; // Временная переменная
+    // меняем нулевой на последний
+    a[i] = a[a.Length-i-1]; // -i что бы не выйти за границы массива
+    a[a.Length-i-1]=tmp; // меняем последний на нулевой
+}
+for (int i=0; i < a.Length; i++)// перебор по всему массиву
+{
+   System.Console.Write(a[i]+" ");
+} 
+*/
+
+//преворот без подпрограммы перворота массива
+
+int n=int.Parse(Console.ReadLine()); // Первая строка
+string s=Console.ReadLine(); //Вторая строка для ввода
+string[] a=s.Split(' ',StringSplitOptions.RemoveEmptyEntries); // Преобразуем строку в массив строк
+//Преобразуем массив строк в массив int
+//int[] a=Array.ConvertAll<string,int>(ss, int.Parse);
+// Решение
+/*
+for (int i=0; i< a.Length/2; i++)
+{
+    string tmp =a[i]; // Временная переменная
+    // меняем нулевой на последний
+    a[i] = a[a.Length-i-1]; // -i что бы не выйти за границы массива
+    a[a.Length-i-1]=tmp; // меняем последний на нулевой
+}
+*/
+for (int i=a.Length-1; i>=0; i--)// перебор по всему массиву
+{
+   System.Console.Write(a[i]+" ");
+}
---
+++ EJUGE/002/Program.cs
@@ -0,0 +1,21 @@
+﻿//Вводиться число N, а затем - N чисел. 
+// Выведите N чисел в следующем порядке:
+// Сначала выводятся все нечетные числа в том порядке,
+// в котором они встречались во входном файле
+// а затем все четные
+// вводиться число N (100>N>0) а затем N чисел
+// из диапазона Integer
+
+//using System; // Пространство имён
+int n=int.Parse(Console.ReadLine()); // Первая строка
+string s=Console.ReadLine(); //Вторая строка для ввода
+// s=s.Trim(); // Удалить пробелы в начале и в конце строки
+string[] ss=s.Split(' ');
+int[] a=Array.ConvertAll<string,int>(ss, int.Parse);
+for (int i=0; i< a.Length; i++)
+    if (a[i] %2 ! = 0)
+    System.Console.WriteLine($"{a[i]}");
+
+for (int j=0; j< a.Length; j++)
+    if (a[j] %2 == 0)
+    System.Console.WriteLine($"{a[j]}");
