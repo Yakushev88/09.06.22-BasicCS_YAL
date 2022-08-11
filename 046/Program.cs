1693030bcd2adcee08573ad838dea20234b26b73
YA88 <lexu4.122@yandex.ru>
2022-08-11 20:57:53 +0500

    Добавил решение задачи №46

---
+++ 046/Program.cs
@@ -0,0 +1,16 @@
+﻿// 46. Показать числа Фибоначчи
+
+Console.WriteLine("До какого порядка вычислять ряд Фибоначчи?");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+Console.Write("{0} ", a);
+int b = 1;
+Console.Write("{0} ", b);
+int sum = 0;
+while (numbers >= sum)
+{
+    sum = a + b;           
+    Console.Write("{0} ", sum);              
+    a = b;
+    b = sum;                
+}
