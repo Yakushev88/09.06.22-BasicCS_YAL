5dd2128192577dfdfd358c3ec677a860536a3f3f
YA88 <lexu4.122@yandex.ru>
2022-08-13 19:27:53 +0500

    Добавили решение задач по двумерным массивам №048.1, №49

---
+++ 048.1/Program.cs
@@ -0,0 +1,24 @@
+﻿// 48. Показать двумерный массив размером m×n 
+// заполненный целыми числами
+int N=2;
+int M = 3;
+// Спарава слева от запятой указание на меры
+// запятых может быть большеб мерность увеличивается 
+int[,] a; // Описываем массив
+a=new int[N,M];//Создаем массив
+// инициализация случайными числами
+Random random=new Random();// Создали объект типа рандом
+for (int i=0;i<a.GetLength(0);i++)
+{
+    for (int j=0;j<a.GetLongLength(1);j++)
+    { 
+        a[i,j]=random.Next(1,11);
+    }
+}
+// Вывод массива на экран
+for (int i=0;i<a.GetLength(0);i++)
+{
+    for (int j=0;j<a.GetLongLength(1);j++) System.Console.Write($"{a[i,j]} ");
+    System.Console.WriteLine();
+}
+
