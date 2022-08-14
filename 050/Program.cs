e88c793d109e7f03b5bb19c21d1c37baeadc7b17
YA88 <lexu4.122@yandex.ru>
2022-08-14 11:06:15 +0500

    Добавили метод принудительного вывода в задаче №50

--- 050/Program.cs
+++ 050/Program.cs
@@ -130,7 +130,7 @@ int[,] Init(int N, int M)
 }
 */
 // !!!!все основные процессы через подпрограммы
-
+/*
 int[,] a; // Описываем массив
 a=Init(3,3);
 Print(a);
@@ -162,6 +162,55 @@ int[,] Init(int N, int M)
         }
     }
     return a;
+}
+// Программа решения
+void Solution(int[,] a)
+{
+    for (int i=0;i<a.GetLength(0);i++)
+    {   // F2- ТОЛЬКО два знака после запятой
+        for (int j=0;j<a.GetLongLength(1);j++) 
+            if (a[i,j]%2==0)
+                a[i,j]=-a[i,j];
+    System.Console.WriteLine();
+    }
+}
+*/
+
+//Что то ещё через подпрограмму
+
+int[,] a; // Описываем массив
+//a=Init(3,3);
+Init2(3,3, out a); // Ввели через принудительный
+// выводб убрав return
+Print(a);
+Solution(a);
+System.Console.WriteLine();
+Print(a); 
+
+// Подпрограмма вывода на экран
+void Print(int[,] a)
+{
+    for (int i=0;i<a.GetLength(0);i++)
+    {   // F2- ТОЛЬКО два знака после запятой
+        for (int j=0;j<a.GetLongLength(1);j++) System.Console.Write($"{a[i,j],4} ");
+        System.Console.WriteLine();
+    }
+}
+
+void Init2(int N, int M, out int[,] a)
+// Программа инициализации массива
+{
+    a=new int[N,M];//Создаем массив
+    // инициализация случайными числами
+    Random random=new Random();// Создали объект типа рандом
+    for (int i=0;i<a.GetLength(0);i++)
+    {
+        for (int j=0;j<a.GetLongLength(1);j++)
+        {   // для увеличения диапазона рандомных чисел
+            // умножаем на 10. Прбавляя 1 смещаем от 1 до 11
+            a[i,j]=random.Next(1,11);
+        }
+    }
 }
 // Программа решения
 void Solution(int[,] a)
