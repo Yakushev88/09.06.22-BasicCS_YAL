85f6c0c84affed6dbbe58c473389062353dccfbf
YA88 <lexu4.122@yandex.ru>
2022-08-20 17:14:36 +0500

    Добавили папкуhanoi, Решение задачи №69

---
+++ 069/Program.cs
@@ -0,0 +1,10 @@
+﻿// Рекурсии
+
+// 69. Найти сумму элементов от M до N, N и M заданы
+
+int P(int M, int N)
+{
+    if (M==N) return M;
+    else return P(M+1,N)+ M;
+}
+System.Console.WriteLine(P(1,10));
--- Recursy/Program.cs
+++ Recursy/Program.cs
@@ -1,5 +1,27 @@
-﻿// 
+﻿// Вывести числа от 1 до N без использования циклов
 
+void Loop(int i, int N)
+{
+    System.Console.WriteLine(i);
+    if (i<N) Loop (i+1,N);
+}    
+int F (int n)
+{
+    if (n>0) return F(n-1)*n;
+    else return 1;
+}
+System.Console.WriteLine(F(5));
+//Loop(1,10);
+
+/*
+int N=15;
+int f=1;
+for (int i=1;i<=N;i++)
+    f=f*i;
+*/ 
+
+
+/*
 int P(int a, int b)
 {
     if (b==0) return 1;
@@ -8,7 +30,7 @@ int P(int a, int b)
 }
 
 System.Console.WriteLine(P(2,10));
-
+*/
 
 /*
 //Вывести числа от 1 до N без использования циклов
