a4b422853b67a8a87d5846f3e02fdb0d81e9f002
YA88 <lexu4.122@yandex.ru>
2022-08-20 10:23:15 +0500

    Добавили решение задачи 042А

--- 029/Program.cs
+++ 029/Program.cs
@@ -1,8 +1,17 @@
 ﻿// Задача №29
 // Подсчитать сумму цифр в числе
 
-// Подпрограмма для посчета цыфр в числе
+int x = int.Parse(Console.ReadLine());
+int sum=0;
+for (int i=3; i>=0; i--)
+{
+    sum+=x/(int)Math.Pow(10.0,i);
+    x=x%(int)Math.Pow(10.0,i);
+}
+Console.WriteLine(sum);
 
+/*
+// Подпрограмма для посчета цыфр в числе
 
 int СчетчикЦыфр(int N) // подпрограма
 {
@@ -18,3 +27,4 @@ return k;
 
  }
  System.Console.WriteLine(СчетчикЦыфр(1876));
+*/
