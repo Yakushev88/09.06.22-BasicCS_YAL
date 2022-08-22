a46c7be9e86b02de3dd89c602620609692d23118
YA88 <lexu4.122@yandex.ru>
2022-08-22 17:30:30 +0500

    Добавил решение задачи №71

---
+++ 071/Program.cs
@@ -0,0 +1,13 @@
+﻿// 71. Написать программу вычисления функции Аккермана
+
+static UInt32 S(uint n, uint m)
+{
+  if (n == 0)
+    return n + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return S(n - 1, m);
+    else
+      return S(n -1, S(n, m -1));
+}
+System.Console.WriteLine(S(4,5));
